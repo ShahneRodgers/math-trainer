08bf2b04dc42b663f4979d4997b4ad8180eceeb7
Anna Lezhikova <anna.v.l@gmail.com>
2016-02-27 18:46:38 +1300

    Move numbers to a mailbox

--- app/Main.elm
+++ app/Main.elm
@@ -6,6 +6,10 @@ import Html exposing(..)
 --MODEL
 
 type alias Model =
+  { score: Int }
+
+
+type alias Math =
   { numA: Int
   , numB: Int
   , operator: String
@@ -16,10 +20,7 @@ initialModel : Model
 initialModel =
   let
     emptyModel =
-      { numA = 0
-      , numB = 0
-      , operator = "-"
-      }
+      { score = 0 }
   in
     Maybe.withDefault emptyModel incoming
 
@@ -34,31 +35,49 @@ update action model =
     NoOp ->
       model
     Check ->
-      { model | numA = 1 }
+      { model | score = model.score + 1 }
 
 
 
 --VIEW
 
-view : Model -> Html
-view model =
+view : Math -> Model -> Html
+view numbers model =
   div []
-    [ text (toString model.numA)
-    , text (model.operator)
-    , text (toString model.numB)
+    [ div
+        []
+        [ text (toString numbers.numA)
+        , text (numbers.operator)
+        , text (toString numbers.numB)
+        ]
+    , div
+        []
+        [ text (toString model.score) ]
     ]
 
 
 --SIGNALS
 
-inbox : Signal.Mailbox Action
-inbox =
+inbox1 : Signal.Mailbox Action
+inbox1 =
   Signal.mailbox NoOp
 
 
 actions : Signal Action
 actions =
-  inbox.signal
+  inbox1.signal
+
+
+inbox2 : Signal.Mailbox Math
+inbox2 =
+  Signal.mailbox  { numA = 0
+                  , numB = 0
+                  , operator = "*"
+                  }
+
+numbers : Signal Math
+numbers =
+  inbox2.signal
 
 
 --PORTS
@@ -79,7 +98,7 @@ model =
 
 main : Signal Html
 main =
-  Signal.map view model
+  Signal.map2 view numbers model
 
 
 
